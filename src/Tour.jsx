56c3df6b41739d91edfc7fa77fc2f1afa56bda3b
abbos91 <abbos.shomurodov91@gmail.com>
2026-07-09 16:40:18 +0500

    read more btn added

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Tour = ({ id, name, image, info, price, removeTour }) => {
+    const [readMore, setReadMore] = useState(false);
     return (
         <article className="single-tour">
             <img src={image} alt={name} className="img" />
             <span className="tour-price">${price}</span>
             <div className="tour-info">
                 <h5>{name}</h5>
-                <p>{info}</p>
+                <p>
+                    {readMore ? info : `${info.substring(0, 200)}...`}{" "}
+                    <button
+                        type="button"
+                        className="info-btn"
+                        onClick={() => {
+                            setReadMore(!readMore);
+                        }}
+                    >
+                        {readMore ? "Show Less" : "Read More"}
+                    </button>{" "}
+                </p>
                 <button
                     type="button"
                     className="btn btn-block delete-btn"
